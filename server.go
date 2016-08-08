7b8f10f93fa5067eb16dd37bab6542c28de6d297
Tri Han <trihan51@yahoo.com>
2016-08-08 01:20:54 -0700

    server is able to process incoming requests and respond. the data exchanged between an external client (mobile app) and the drone server is in json format

--- server.go
+++ server.go
@@ -3,14 +3,45 @@ package main
 import (
   "fmt"
   "net/http"
+  "io/ioutil"
+  "encoding/json"
 )
+
+type Message struct {
+     Command string "json:\"command\""
+     Response string "json:\"response\""
+}
 
 func defaultHandler(w http.ResponseWriter, r *http.Request) {
   fmt.Fprintf(w, "path: %s. Welcome to the Drone's server.", r.URL.Path[:])
 }
 
 func connectHandler(w http.ResponseWriter, r *http.Request) {
-  fmt.Fprintf(w, "path: %s. Welcome to the Connect path.", r.URL.Path[:])
+  fmt.Println("A device has contacted this drone server.")
+  fmt.Println("Preparing to handle request.")
+
+  p, err := ioutil.ReadAll(r.Body)
+  if err != nil {
+     panic(err)
+  }
+
+  var m Message
+  err = json.Unmarshal(p, &m)
+
+  if err != nil {
+    panic(err)
+  }
+
+  fmt.Printf("Client request: %s", m.Command)
+
+  responseMessage := Message{"No command", "Successful"}
+  b, err := json.Marshal(responseMessage)
+
+  if err != nil {
+    panic(err)
+  }
+
+  w.Write(b)
 }
 
 func commandsHandler(w http.ResponseWriter, r *http.Request) {
